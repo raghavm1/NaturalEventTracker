aa24b15beb3d4bab531f9a1740af5d2c238bf8c0
Raghav Mantri <mantriraghav11@gmail.com>
2020-11-22 16:41:30 +0800

    modified font and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <header className="header">
-            <h1>Wildfire tracker</h1>
+            <h1>Wildfire tracker, powered by <a href="https://api.nasa.gov/">NASA</a></h1>
         </header>
     )
 }
